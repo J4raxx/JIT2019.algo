a9e50dc5669cc1c9b44f4e23c8832f2e5eb7ff0d
J4raxx <jaraxxeu@gmail.com>
2019-09-28 15:39:18 +0300

    classified

--- sharpening.py
+++ sharpening.py
@@ -9,41 +9,46 @@ from os.path import isfile, join
 
 
 
+class sharpening(object):
+    def __init__(self, path):
+        self.path = path
+      
+    def calculate (self, pathToFile):
+        img = Image.open(self.path+'/'+pathToFile)
+        bright = []
+        pix = img.load()
+        width = img.size[0]
+        height = img.size[1]
+        print (width, " ", height)
+        for i in range(0, width):
+            temp = []
+            for j in range(0, height):
+                temp.append(pix[(i, j)][0]*0.2126 + pix[(i, j)][1]*0.5786 + pix[(i, j)][2]*0.0072)
+            bright.append(temp)
 
-path = ""
-
-
-def calculate(pathToFile):
-	global path
-	img = Image.open(path+'/'+pathToFile)
-	bright = []
-	pix = img.load()
-	width = img.size[0]
-	height = img.size[1]
-	print (width, " ", height)
-	for i in range(0, width):
-		temp = []
-		for j in range(0, height):
-			temp.append(pix[(i, j)][0]*0.2126 + pix[(i, j)][1]*0.5786 + pix[(i, j)][2]*0.0072)
-		bright.append(temp)
-
-	#Расчет коэффициента резкости
-	RQ = 0.0
-	K = 255.0
-	for i in range (1, width):
-		for j in range(1, height):
-			r = (abs(bright[i][j]-bright[i-1][j]) + abs(bright[i][j]-bright[i][j-1]))
-			RQ += ( r*r/((width - 1)*(height-1)*K))
-	return RQ
-
+        #Расчет коэффициента резкости
+        RQ = 0.0
+        K = 255.0
+        for i in range (1, width):
+            for j in range(1, height):
+                r = (abs(bright[i][j]-bright[i-1][j]) + abs(bright[i][j]-bright[i][j-1]))
+                RQ += ( r*r/((width - 1)*(height-1)*K))
+        print(RQ)
+        return RQ
+    def solve(self):
+        onlyfiles = [f for f in listdir(self.path) if isfile(join(self.path, f))]
+        maxI = 0
+        maxRQ = self.calculate(onlyfiles[0])
+        for i in range(1, len(onlyfiles)):
+            k = self.calculate(onlyfiles[i])
+            if k > maxRQ:
+                maxRQ = k
+                maxI = i
+        return onlyfiles[maxI]
+          
 
 
 #открытие фотографии (test)
-"""
-root = tk.Tk()
-root.withdraw()
-
-path = filedialog.askdirectory()
 """
 maxI = 0
 
@@ -60,3 +65,14 @@ def calcAll(onlyfiles):
             maxRQ = k
             maxI = i
     return onlyfiles[i]
+"""
+
+
+root = tk.Tk()
+root.withdraw()
+
+yoooo = filedialog.askdirectory()
+
+
+obj = sharpening(yoooo)
+print (obj.solve())
